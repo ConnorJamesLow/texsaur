23678a0efae0d806109d1c55619b9453bd9df85f
Connor Low <connor.low@avalara.com>
2023-02-22 15:43:49 -0800

    Was gonna make a sweet example, but first I'll let vite address this breaking issue I found

---
+++ examples/vite-custom-element/site/src/main.ts
@@ -0,0 +1,4 @@
+import page from './page'
+
+console.log('hello there')
+document.querySelector('#app')!.appendChild(page);
---
+++ examples/vite-custom-element/site/src/page.tsx
@@ -0,0 +1,7 @@
+import jsx from 'texsaur';
+
+export default (
+    <main>
+        <h1>Hello there</h1>
+    </main>
+);
