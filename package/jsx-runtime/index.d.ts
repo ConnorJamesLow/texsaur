00f4648fb5d2127ab2d612468adbdbe5fe099207
Connor Low <connorjameslow@gmail.com>
2026-04-12 18:05:53 -0700

    Add jsx-dev-runtime

--- package/jsx-runtime/index.d.ts
+++ package/jsx-runtime/index.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="../types/jsx.d.ts" />
 export { default as jsx } from '../src';
+export { default as jsxDEV } from '../src';
--- package/jsx-runtime/index.js
+++ package/jsx-runtime/index.js
@@ -3,4 +3,7 @@ export const jsx = (t, p) => __jsx(t, Object.fromEntries(Object.entries(p)
     .filter(([n]) => n !== 'children')
 ), ...(Array.isArray(p.children) ? p.children : [p.children || '']));
 export const jsxs = jsx;
+export const jsxDEV = (t, p, k, _i, _s, _S) => {
+  return jsx(t, p, k);
+};
 export const Fragment = __jsx.Fragment;
